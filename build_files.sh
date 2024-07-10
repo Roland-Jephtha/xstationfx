0aa311f97c15ff69c56851d936590450b6dea1c7
unknown <jephtroland832@gmail.com>
2024-07-11 00:59:28 +0100

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,30 @@
+
+
+#!/bin/bash
+
+# Create a virtual environment
+python3.9 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install dependencies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Deactivate the virtual environment
+deactivate
+
+
+
+
+
+
+
+# pip install -r requirements.txt
+# python3.9 manage.py collectstatic
